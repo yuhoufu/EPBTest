91e2c462ddc1bb8512dcf415e28ef0fbe1f61c9c
yuhoufu <1981250700@qq.com>
2025-12-02 17:33:44 +0800

    忽略一些编译提示

--- Config/ConfigLoader.cs
+++ Config/ConfigLoader.cs
@@ -251,7 +251,7 @@ public static class ConfigLoader
         cfg.MinPercent = GetDouble(doc, "//AOConfig/MinPercent", 0);
         cfg.MaxPercent = GetDouble(doc, "//AOConfig/MaxPercent", 100);
 
-        foreach (XmlNode n in doc.SelectNodes("//AOConfig/Devices/Device"))
+        foreach (XmlNode n in doc.SelectNodes("//AOConfig/Devices/Device")!)
         {
             var d = new AoDevice
             {
@@ -260,7 +260,7 @@ public static class ConfigLoader
                 ScaleK = GetDouble(n, "ScaleK", 1.0),
                 Offset = GetDouble(n, "Offset", 0.0)
             };
-            foreach (XmlNode p in n.SelectNodes("PercentToPressureTable/Point"))
+            foreach (XmlNode p in n.SelectNodes("PercentToPressureTable/Point")!)
                 d.PercentToPressure.Add((GetDouble(p, "Percent", 0), GetDouble(p, "Pressure", 0)));
 
             if (!string.IsNullOrEmpty(d.Name)) cfg.Devices[d.Name] = d;
@@ -278,7 +278,7 @@ public static class ConfigLoader
         var doc = new XmlDocument();
         doc.Load(path);
 
-        foreach (XmlNode n in doc.SelectNodes("//DOConfig/EPB/Record"))
+        foreach (XmlNode n in doc.SelectNodes("//DOConfig/EPB/Record")!)
         {
             var r = new DoEpbRecord
             {
