450cd6af4185d90075666f74b4866e4dc2aaf70d
yuhoufu <1981250700@qq.com>
2026-08-05 12:36:07 +0800

    chore: 升级主程序修订版本至2.10.2.1

--- MTTfTest/Properties/AssemblyInfo.cs
+++ MTTfTest/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.10.2.0")]
-[assembly: AssemblyFileVersion("2.10.2.0")]
-[assembly: AssemblyInformationalVersion("2.10.2")]
+[assembly: AssemblyVersion("2.10.2.1")]
+[assembly: AssemblyFileVersion("2.10.2.1")]
+[assembly: AssemblyInformationalVersion("2.10.2.1")]
