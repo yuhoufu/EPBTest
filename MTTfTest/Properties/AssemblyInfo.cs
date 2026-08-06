12f2fd1255369ce0d2adace96ab24fd8b294420f
yuhoufu <1981250700@qq.com>
2026-08-06 15:43:08 +0800

    chore: 发布程序版本2.11.0.0

--- MTTfTest/Properties/AssemblyInfo.cs
+++ MTTfTest/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.10.2.11")]
-[assembly: AssemblyFileVersion("2.10.2.11")]
-[assembly: AssemblyInformationalVersion("2.10.2.11")]
+[assembly: AssemblyVersion("2.11.0.0")]
+[assembly: AssemblyFileVersion("2.11.0.0")]
+[assembly: AssemblyInformationalVersion("2.11.0.0")]
--- Tools/Build-Release.ps1
+++ Tools/Build-Release.ps1
@@ -62,7 +62,7 @@ $manifestFiles = foreach ($file in $files) {
     }
 }
 $identity = [ordered]@{
-    productVersion = 'V2.10.2.11'
+    productVersion = 'V2.11.0.0'
     gitCommit = $commit
     gitBranch = $branch
     gitDirty = $false
