f25af7c4d05cdcc3d78691d85a5f1e81f91b2717
yuhoufu <1981250700@qq.com>
2026-08-07 11:27:07 +0800

    chore: 标识程序版本2.11.0.9

--- MTTfTest/Properties/AssemblyInfo.cs
+++ MTTfTest/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.11.0.8")]
-[assembly: AssemblyFileVersion("2.11.0.8")]
-[assembly: AssemblyInformationalVersion("2.11.0.8")]
+[assembly: AssemblyVersion("2.11.0.9")]
+[assembly: AssemblyFileVersion("2.11.0.9")]
+[assembly: AssemblyInformationalVersion("2.11.0.9")]
