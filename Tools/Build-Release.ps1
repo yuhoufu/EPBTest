4067194371e0b468a6784814728130a9a8b36e3e
yuhoufu <1981250700@qq.com>
2026-08-07 18:22:37 +0800

    chore: 标识程序版本2.11.0.14

--- Tools/Build-Release.ps1
+++ Tools/Build-Release.ps1
@@ -62,7 +62,7 @@ $manifestFiles = foreach ($file in $files) {
     }
 }
 $identity = [ordered]@{
-    productVersion = 'V2.11.0.12'
+    productVersion = 'V2.11.0.14'
     gitCommit = $commit
     gitBranch = $branch
     gitDirty = $false
