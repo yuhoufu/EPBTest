02498735269ad81bce24ccef6ec036eb7b1e33b1
yuhoufu <1981250700@qq.com>
2026-08-27 16:22:37 +0800

    chore: 标识程序版本2.13.0.28

--- Tests/AdaptiveControlTests/StrictHostV4ProductionTests.cs
+++ Tests/AdaptiveControlTests/StrictHostV4ProductionTests.cs
@@ -21,7 +21,7 @@ namespace AdaptiveControlTests
         internal static int RunAll()
         {
             var passed = 0;
-            Run("V2.13.0.26 watchdog assembly identity", WatchdogAssemblyVersionIdentity, ref passed);
+            Run("V2.13.0.28 watchdog assembly identity", WatchdogAssemblyVersionIdentity, ref passed);
             Run("strict bootstrap format2", StrictBootstrapFormat2, ref passed);
             Run("approved intent durable", ApprovedToIntent, ref passed);
             Run("approved permit取消后耐久Superseded且重启不可消费", ApprovedPermitRevocationIsDurable, ref passed);
@@ -46,13 +46,13 @@ namespace AdaptiveControlTests
 
         private static void WatchdogAssemblyVersionIdentity()
         {
-            var expected = new Version(2, 13, 0, 26);
+            var expected = new Version(2, 13, 0, 28);
             Require(typeof(WatchdogProtocol).Assembly.GetName().Version == expected,
-                "Protocol assembly version is not V2.13.0.26");
+                "Protocol assembly version is not V2.13.0.28");
             Require(typeof(WatchdogClientTransportEngine).Assembly.GetName().Version == expected,
-                "Client assembly version is not V2.13.0.26");
+                "Client assembly version is not V2.13.0.28");
             Require(typeof(StrictHostV4AuthorityAdapter).Assembly.GetName().Version == expected,
-                "Host assembly version is not V2.13.0.26");
+                "Host assembly version is not V2.13.0.28");
             Require(WatchdogProtocol.Version == 3 &&
                     WatchdogJournalPolicy.CurrentSchemaVersion == 4 &&
                     DurableRelaunchAuthorityV4Validator.RequiredFormatRevision == 2,
