a4ee2c6b0ac6980eebd4e7a2f666cc973bc297d2
yuhoufu <1981250700@qq.com>
2026-08-30 13:47:45 +0800

    chore(version): 标识V2.13.0.39程序版本

--- Tests/AdaptiveControlTests/StrictHostV4ProductionTests.cs
+++ Tests/AdaptiveControlTests/StrictHostV4ProductionTests.cs
@@ -23,7 +23,7 @@ namespace AdaptiveControlTests
         internal static int RunAll()
         {
             var passed = 0;
-            Run("V2.13.0.38 release assembly identity", WatchdogAssemblyVersionIdentity, ref passed);
+            Run("V2.13.0.39 release assembly identity", WatchdogAssemblyVersionIdentity, ref passed);
             Run("strict bootstrap format2", StrictBootstrapFormat2, ref passed);
             Run("approved intent durable", ApprovedToIntent, ref passed);
             Run("approved permit取消后耐久Superseded且重启不可消费", ApprovedPermitRevocationIsDurable, ref passed);
@@ -51,18 +51,18 @@ namespace AdaptiveControlTests
 
         private static void WatchdogAssemblyVersionIdentity()
         {
-            var expected = new Version(2, 13, 0, 38);
+            var expected = new Version(2, 13, 0, 39);
             Require(typeof(WatchdogProtocol).Assembly.GetName().Version == expected,
-                "Protocol assembly version is not V2.13.0.38");
+                "Protocol assembly version is not V2.13.0.39");
             Require(typeof(WatchdogClientTransportEngine).Assembly.GetName().Version == expected,
-                "Client assembly version is not V2.13.0.38");
+                "Client assembly version is not V2.13.0.39");
             Require(typeof(StrictHostV4AuthorityAdapter).Assembly.GetName().Version == expected,
-                "Host assembly version is not V2.13.0.38");
+                "Host assembly version is not V2.13.0.39");
             Require(typeof(Main_Frm).Assembly.GetName().Version == expected,
-                "Main application assembly version is not V2.13.0.38");
+                "Main application assembly version is not V2.13.0.39");
             Require(typeof(EpbManager).Assembly.GetName().Version == expected,
-                "Controller assembly version is not V2.13.0.38");
-            Require(WatchdogProtocol.Version == 3 &&
+                "Controller assembly version is not V2.13.0.39");
+            Require(WatchdogProtocol.Version == 4 &&
                     WatchdogJournalPolicy.CurrentSchemaVersion == 4 &&
                     DurableRelaunchAuthorityV4Validator.RequiredFormatRevision == 2,
                 "wire/schema/record compatibility tuple changed");
--- Watchdog.Client/Properties/AssemblyInfo.cs
+++ Watchdog.Client/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("MTTFTest.Watchdog.Client")]
 [assembly: AssemblyDescription("Watchdog client transport engine")]
@@ -7,6 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("MTTFTest")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4E6B3F5E-7C42-4A1C-9E02-7D9A3D4C8E61")]
-[assembly: AssemblyVersion("2.13.0.38")]
-[assembly: AssemblyFileVersion("2.13.0.38")]
-[assembly: AssemblyInformationalVersion("2.13.0.38")]
+[assembly: InternalsVisibleTo("AdaptiveControlTests")]
+[assembly: AssemblyVersion("2.13.0.39")]
+[assembly: AssemblyFileVersion("2.13.0.39")]
+[assembly: AssemblyInformationalVersion("2.13.0.39")]
