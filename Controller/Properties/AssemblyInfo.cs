e5930ee945f6597f8a5bb94e35721fa79bb5561d
yuhoufu <1981250700@qq.com>
2026-08-31 23:17:47 +0800

    chore(version): 标识V2.13.0.45程序版本

--- Controller/Properties/AssemblyInfo.cs
+++ Controller/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.13.0.44")]
-[assembly: AssemblyFileVersion("2.13.0.44")]
-[assembly: AssemblyInformationalVersion("2.13.0.44")]
+[assembly: AssemblyVersion("2.13.0.45")]
+[assembly: AssemblyFileVersion("2.13.0.45")]
+[assembly: AssemblyInformationalVersion("2.13.0.45")]
 [assembly: InternalsVisibleTo("AdaptiveControlTests")]
 [assembly: InternalsVisibleTo("MTTFTest")]
