07451deb4c692256ef0778605c71a1367a5c112a
yuhoufu <1981250700@qq.com>
2026-08-31 13:24:15 +0800

    chore(version): 标识V2.13.0.43程序版本

--- Controller/Properties/AssemblyInfo.cs
+++ Controller/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.13.0.42")]
-[assembly: AssemblyFileVersion("2.13.0.42")]
-[assembly: AssemblyInformationalVersion("2.13.0.42")]
+[assembly: AssemblyVersion("2.13.0.43")]
+[assembly: AssemblyFileVersion("2.13.0.43")]
+[assembly: AssemblyInformationalVersion("2.13.0.43")]
 [assembly: InternalsVisibleTo("AdaptiveControlTests")]
 [assembly: InternalsVisibleTo("MTTFTest")]
--- Tests/AdaptiveControlTests/StrictHostV4ProductionTests.cs
+++ Tests/AdaptiveControlTests/StrictHostV4ProductionTests.cs
@@ -23,7 +23,7 @@ namespace AdaptiveControlTests
         internal static int RunAll()
         {
             var passed = 0;
-            Run("V2.13.0.42 release assembly identity", WatchdogAssemblyVersionIdentity, ref passed);
+            Run("V2.13.0.43 release assembly identity", WatchdogAssemblyVersionIdentity, ref passed);
             Run("strict bootstrap format2", StrictBootstrapFormat2, ref passed);
             Run("approved intent durable", ApprovedToIntent, ref passed);
             Run("approved permit取消后耐久Superseded且重启不可消费", ApprovedPermitRevocationIsDurable, ref passed);
@@ -51,17 +51,17 @@ namespace AdaptiveControlTests
 
         private static void WatchdogAssemblyVersionIdentity()
         {
-            var expected = new Version(2, 13, 0, 42);
+            var expected = new Version(2, 13, 0, 43);
             Require(typeof(WatchdogProtocol).Assembly.GetName().Version == expected,
-                "Protocol assembly version is not V2.13.0.42");
+                "Protocol assembly version is not V2.13.0.43");
             Require(typeof(WatchdogClientTransportEngine).Assembly.GetName().Version == expected,
-                "Client assembly version is not V2.13.0.42");
+                "Client assembly version is not V2.13.0.43");
             Require(typeof(StrictHostV4AuthorityAdapter).Assembly.GetName().Version == expected,
-                "Host assembly version is not V2.13.0.42");
+                "Host assembly version is not V2.13.0.43");
             Require(typeof(Main_Frm).Assembly.GetName().Version == expected,
-                "Main application assembly version is not V2.13.0.42");
+                "Main application assembly version is not V2.13.0.43");
             Require(typeof(EpbManager).Assembly.GetName().Version == expected,
-                "Controller assembly version is not V2.13.0.42");
+                "Controller assembly version is not V2.13.0.43");
             Require(WatchdogProtocol.Version == 4 &&
                     WatchdogJournalPolicy.CurrentSchemaVersion == 4 &&
                     DurableRelaunchAuthorityV4Validator.RequiredFormatRevision == 2,
