4b9a81b68321b60bf205b977bec11f1c28d90019
yuhoufu <1981250700@qq.com>
2026-09-02 21:12:51 +0800

    chore(release): 标识V2.14.2.8版本

--- MTTfTest/Properties/AssemblyInfo.cs
+++ MTTfTest/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //      生成号
 //      修订号
 //
-[assembly: AssemblyVersion("2.14.2.7")]
-[assembly: AssemblyFileVersion("2.14.2.7")]
-[assembly: AssemblyInformationalVersion("2.14.2.7")]
+[assembly: AssemblyVersion("2.14.2.8")]
+[assembly: AssemblyFileVersion("2.14.2.8")]
+[assembly: AssemblyInformationalVersion("2.14.2.8")]
